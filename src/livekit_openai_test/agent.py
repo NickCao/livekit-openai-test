c25ddc8bce78a5281590fc365b8bae493fc9a08f
Nick Cao <ncao@redhat.com>
2026-09-22 12:40:40 -0400

    add dummy agent

---
+++ src/livekit_openai_test/agent.py
@@ -0,0 +1,34 @@
+from livekit import agents
+from livekit.agents import Agent, AgentServer, AgentSession, room_io
+from livekit.plugins import (
+    openai,
+)
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions="You are a helpful voice AI assistant.")
+
+
+server = AgentServer()
+
+
+@server.rtc_session(agent_name="my-agent")
+async def my_agent(ctx: agents.JobContext):
+    session = AgentSession(llm=openai.realtime.RealtimeModel(voice="coral"))
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_options=room_io.RoomOptions(
+            audio_input=room_io.AudioInputOptions(),
+        ),
+    )
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance. You should start by speaking in English."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(server)
