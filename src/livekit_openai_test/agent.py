4dac67f5d42dcf4a146cb05d95cc74c4d2d51c28
Nick Cao <ncao@redhat.com>
2026-09-22 13:17:14 -0400

    disable turn detection

--- src/livekit_openai_test/agent.py
+++ src/livekit_openai_test/agent.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from livekit import agents
 from livekit.agents import Agent, AgentServer, AgentSession, room_io
 from livekit.plugins import openai
+from openai.types.beta.realtime.session import TurnDetection
 
 load_dotenv()
 
@@ -16,7 +17,11 @@ server = AgentServer()
 
 @server.rtc_session(agent_name="my-agent")
 async def my_agent(ctx: agents.JobContext):
-    session = AgentSession(llm=openai.realtime.RealtimeModel(voice="coral"))
+    session = AgentSession(
+        llm=openai.realtime.RealtimeModel(
+            turn_detection=None,
+        )
+    )
 
     await session.start(
         room=ctx.room,
@@ -24,10 +29,6 @@ async def my_agent(ctx: agents.JobContext):
         room_options=room_io.RoomOptions(
             audio_input=room_io.AudioInputOptions(),
         ),
-    )
-
-    await session.generate_reply(
-        instructions="Greet the user and offer your assistance. You should start by speaking in English."
     )
 
 
